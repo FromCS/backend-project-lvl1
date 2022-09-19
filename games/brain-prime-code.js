e1beaeac2307eae126a4e1a73a8413d92f3ee594
Mikhail Gostjukhin <mkg0st@yandex.ru>
2022-09-19 20:38:08 +0300

    add games/brain-prime-code.js

---
+++ games/brain-prime-code.js
@@ -0,0 +1,20 @@
+const printPrimeRules = () => console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+const getPrimeQuestion = () => {
+    const randomNumber = Math.round(Math.random()*100) + 1;
+    return randomNumber;
+};
+
+const getPrimeCorrectAnswer = (task) => {
+    const primeNumberDivisors = `${task}, 1`;
+    const divisors = [];
+    for (let i = task; i >= 1; i -= 1) {
+        if (task % i === 0) {
+            divisors.push(i);
+        }
+    }
+    const isPrime = divisors.join(', ') === primeNumberDivisors ? 'yes' : 'no';
+    return isPrime;
+};
+
+export {printPrimeRules, getPrimeQuestion, getPrimeCorrectAnswer};
