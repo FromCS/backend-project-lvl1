82efc7ea66acef8eba981138dc96524fda17b302
Mikhail Gostjukhin <mkg0st@yandex.ru>
2022-09-19 20:36:31 +0300

    add games/brain-calc-code.js

---
+++ games/brain-calc-code.js
@@ -0,0 +1,18 @@
+const printCalcRules = () => console.log('What is the result of the expression?');
+
+const getCalcQuestion = () => {
+    const operations = ['+','-','*'];
+    const firstNumber = Math.round(Math.random()*25);
+    const secondNumber = Math.round(Math.random()*25);
+    const operation = operations[Math.round(Math.random()*(operations.length-1))];
+    const expression = `${firstNumber} ${operation} ${secondNumber}`;
+    return expression;
+};
+
+const getCalcCorrectAnswer = (task) => {
+    const correctAnswer = eval(task).toString();
+    return correctAnswer;
+};
+
+
+export {printCalcRules, getCalcQuestion, getCalcCorrectAnswer};
