1aa77778ffbcbad8e2c285fe507412bd7f3dc31e
Mikhail Gostjukhin <mkg0st@yandex.ru>
2022-09-20 22:11:35 +0300

    fix brain-gcd-code.js

--- games/brain-gcd-code.js
+++ games/brain-gcd-code.js
@@ -11,15 +11,15 @@ const getGcdCorrectAnswer = (task) => {
     const numbers = task.split(' ');
     const [firstNumber, secondNumber] = numbers;
     let firstNumberDivisors = [];
-    for (let i = firstNumber; i >= 1; i -= 1) {
+    for (let i = Number(firstNumber); i >= 1; i -= 1) {
         if (firstNumber % i === 0) {
             firstNumberDivisors.push(i);
         }
     }
-
-    for (let j = secondNumber; j >= 1; j -= 1) {
-        if (secondNumber % j === 0 && firstNumberDivisors.includes(j)) {
-            return j;
+    
+    for (let j = Number(secondNumber); j >= 1; j -= 1) {
+        if ((Number(secondNumber) % j === 0) && firstNumberDivisors.includes(j)) {
+            return String(j);
         }
     }
 };
